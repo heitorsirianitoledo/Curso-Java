525511d41d953bc99f548a093da3baaf87d9d732
heitorsirianitoledo <heitorsiriani06@gmail.com>
2025-09-26 01:14:28 -0300

    Exercicio 25 finalizado

--- curso.java/exercicios/Exercicio25.java
+++ curso.java/exercicios/Exercicio25.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Department;
+import entities.HourContract;
 import entities.Worker;
 import entities.WorkerLevel;
 
@@ -28,15 +29,28 @@ public class Exercicio25 {
 
         System.out.println("How many contracts to this worker: ");
         int n = sc.nextInt();
-        for(int i = 0;i<=n;i++){
+        for(int i = 0; i<n; i++){
             System.out.println("Enter Contract #" + (i+1) + ":");
             System.out.println("Date (DD/MM/YY): ");
             Date contractDate = sdf.parse(sc.next());
             System.out.println("Value per hour: ");
             Double valuePerHour = sc.nextDouble();
-            
+            System.out.println("Duration(hours): ");
+            int hours = sc.nextInt();
+            HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contract);
 
         }
+
+        System.out.println("Enter month and year to calculate income(MM/YY)");
+        String monthAndYear = sc.next();
+        int month = Integer.parseInt(monthAndYear.substring(0,2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
+        System.out.println("Name: " + workerName);
+        System.out.println("Department: " + departmentName);
+        System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
+
+
 
 
 
